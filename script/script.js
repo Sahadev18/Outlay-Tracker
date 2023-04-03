72db19cf728157b4ae556d21c0e2b242f4b44a6f
Sahadev Bhaganagare <sahadevbh8@gmail.com>
2023-04-03 22:12:00 +0530

    lst commit

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ bar.addEventListener('click', function ale(){
     const nav=document.getElementById("nav")
     const main=document.getElementById("main")
     if(profile.style.display==="none"){
-        profile.style.display="block"
+        profile.style.display="flex"
         nav.style.display="block"
         head.style.gridColumn="2/7"
         main.style.gridColumn="2/7"
@@ -18,3 +18,12 @@ bar.addEventListener('click', function ale(){
         main.style.gridColumn="1/7"
     }
 })
+
+function login(){
+    var email=document.getElementById("email").value
+    var pass=document.getElementById("pass").value
+    if(pass=="admin"){
+        const win1=window.open('file:///home/sahadev/Desktop/coding/webdev/proj2/html/index.html')
+        win1.document.getElementById("name").innerHTML="Admin"
+    }
+}
